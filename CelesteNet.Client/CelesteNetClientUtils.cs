dd6d6dcaf09f9c649ff3baeeae85b751f0972aad
Maik Macho <nuda1998@gmail.com>
2020-06-08 17:02:59 +0200

    Fix compiling with XNA and UI blur on FNA

--- CelesteNet.Client/CelesteNetClientUtils.cs
+++ CelesteNet.Client/CelesteNetClientUtils.cs
@@ -1,7 +1,13 @@
-﻿using Celeste.Mod.CelesteNet.DataTypes;
+﻿using MC = Mono.Cecil;
+using CIL = Mono.Cecil.Cil;
+
+using Celeste.Mod.CelesteNet.DataTypes;
 using Celeste.Mod.Helpers;
 using Microsoft.Xna.Framework;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
 using Monocle;
+using MonoMod.Utils;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -26,6 +32,128 @@ namespace Celeste.Mod.CelesteNet.Client {
 
         public static bool GetUpdateHair(this Level self)
             => (bool) f_Level_updateHair.GetValue(self);
+
+        private delegate IntPtr _AsPointer<T>(ref T value);
+        private static readonly Dictionary<Type, Delegate> _AsPointerCache = new Dictionary<Type, Delegate>();
+        private static MethodInfo _AsPointerHelper;
+        public static IntPtr AsPointer<T>(ref T value) {
+            Delegate cached;
+            lock (_AsPointerCache)
+                _AsPointerCache.TryGetValue(typeof(T), out cached);
+            if (cached != null)
+                return (cached as _AsPointer<T>)(ref value);
+
+            if (_AsPointerHelper == null) {
+                Assembly asm;
+
+                const string @namespace = "Celeste.Mod.CelesteNet.Client";
+                const string @name = "AsPointerHelper";
+                const string @fullName = @namespace + "." + @name;
+
+                using (ModuleDefinition module = ModuleDefinition.CreateModule(
+                    @fullName,
+                    new ModuleParameters() {
+                        Kind = ModuleKind.Dll,
+                        ReflectionImporterProvider = MMReflectionImporter.Provider
+                    }
+                )) {
+
+                    TypeDefinition type = new TypeDefinition(
+                        @namespace,
+                        @name,
+                        MC.TypeAttributes.Public | MC.TypeAttributes.Abstract | MC.TypeAttributes.Sealed
+                    ) {
+                        BaseType = module.TypeSystem.Object
+                    };
+                    module.Types.Add(type);
+
+                    MethodDefinition method = new MethodDefinition(@name,
+                        MC.MethodAttributes.Public | MC.MethodAttributes.Static | MC.MethodAttributes.HideBySig,
+                        module.TypeSystem.Int32
+                    );
+                    GenericParameter genParam = new GenericParameter("T", method);
+                    method.GenericParameters.Add(genParam);
+                    method.Parameters.Add(new ParameterDefinition("value", MC.ParameterAttributes.None, new ByReferenceType(genParam)));
+                    type.Methods.Add(method);
+
+                    ILProcessor il = method.Body.GetILProcessor();
+                    il.Emit(OpCodes.Ldarg_0);
+                    il.Emit(OpCodes.Conv_U);
+                    il.Emit(OpCodes.Ret);
+
+                    asm = ReflectionHelper.Load(module);
+                }
+
+                _AsPointerHelper = asm.GetType(@fullName).GetMethod(@name);
+            }
+
+            _AsPointer<T> generated = _AsPointerHelper.MakeGenericMethod(typeof(T)).CreateDelegate<_AsPointer<T>>() as _AsPointer<T>;
+            lock (_AsPointerCache)
+                _AsPointerCache[typeof(T)] = generated;
+            return generated(ref value);
+        }
+
+        private delegate ref T _AsRef<T>(IntPtr value);
+        private static readonly Dictionary<Type, Delegate> _AsRefCache = new Dictionary<Type, Delegate>();
+        private static MethodInfo _AsRefHelper;
+        public static ref T AsRef<T>(IntPtr value) {
+            Delegate cached;
+            lock (_AsRefCache)
+                _AsRefCache.TryGetValue(typeof(T), out cached);
+            if (cached != null)
+                return ref (cached as _AsRef<T>)(value);
+
+            if (_AsRefHelper == null) {
+                Assembly asm;
+
+                const string @namespace = "Celeste.Mod.CelesteNet.Client";
+                const string @name = "AsRefHelper";
+                const string @fullName = @namespace + "." + @name;
+
+                using (ModuleDefinition module = ModuleDefinition.CreateModule(
+                    @fullName,
+                    new ModuleParameters() {
+                        Kind = ModuleKind.Dll,
+                        ReflectionImporterProvider = MMReflectionImporter.Provider
+                    }
+                )) {
+
+                    TypeDefinition type = new TypeDefinition(
+                        @namespace,
+                        @name,
+                        MC.TypeAttributes.Public | MC.TypeAttributes.Abstract | MC.TypeAttributes.Sealed
+                    ) {
+                        BaseType = module.TypeSystem.Object
+                    };
+                    module.Types.Add(type);
+
+                    MethodDefinition method = new MethodDefinition(@name,
+                        MC.MethodAttributes.Public | MC.MethodAttributes.Static | MC.MethodAttributes.HideBySig,
+                        module.TypeSystem.Int32
+                    );
+                    GenericParameter genParam = new GenericParameter("T", method);
+                    method.GenericParameters.Add(genParam);
+                    method.Parameters.Add(new ParameterDefinition("value", MC.ParameterAttributes.None, new ByReferenceType(module.TypeSystem.Int32)));
+                    method.Body.Variables.Add(new VariableDefinition(new ByReferenceType(genParam)));
+                    type.Methods.Add(method);
+
+                    ILProcessor il = method.Body.GetILProcessor();
+                    il.Emit(OpCodes.Ldarg_0);
+                    il.Emit(OpCodes.Stloc_0);
+                    il.Emit(OpCodes.Ldloc_0);
+                    il.Emit(OpCodes.Ret);
+
+                    asm = ReflectionHelper.Load(module);
+                }
+
+                _AsRefHelper = asm.GetType(@fullName).GetMethod(@name);
+            }
+
+            _AsRef<T> generated = _AsRefHelper.MakeGenericMethod(typeof(T)).CreateDelegate<_AsRef<T>>() as _AsRef<T>;
+            lock (_AsRefCache)
+                _AsRefCache[typeof(T)] = generated;
+            return ref generated(value);
+        }
 
     }
 }
--- CelesteNet.Client/Components/CelesteNetBlurHelperComponent.cs
+++ CelesteNet.Client/Components/CelesteNetBlurHelperComponent.cs
@@ -2,30 +2,30 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Mono.Cecil.Cil;
 using Monocle;
+using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using MDraw = Monocle.Draw;
 
 namespace Celeste.Mod.CelesteNet.Client.Components {
-    public class CelesteNetBlurHelperComponent : CelesteNetGameComponent {
+    public unsafe class CelesteNetBlurHelperComponent : CelesteNetGameComponent {
 
         public const int BLUR_SCALE = 3;
         public const int BLUR_WIDTH = UI_WIDTH / BLUR_SCALE;
         public const int BLUR_HEIGHT = UI_HEIGHT / BLUR_SCALE;
 
         public RenderTarget2D RTGame;
-        public bool ForceRTGameAsBackbuffer;
 
         public RenderTarget2D RTBlurX;
         public RenderTarget2D RTBlurY;
         public RenderTarget2D RTBlur;
-
-        private Hook h_SetRenderTarget;
 
         public CelesteNetBlurHelperComponent(CelesteNetClientComponent context, Game game)
             : base(context, game) {
@@ -58,22 +58,15 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
             RTBlurY = new RenderTarget2D(GraphicsDevice, BLUR_WIDTH, BLUR_HEIGHT, false, SurfaceFormat.Color, DepthFormat.None, 0, RenderTargetUsage.DiscardContents);
             RTBlur = new RenderTarget2D(GraphicsDevice, UI_WIDTH, UI_HEIGHT, false, SurfaceFormat.Color, DepthFormat.None, 0, RenderTargetUsage.DiscardContents);
 
-            On.Celeste.Celeste.RenderCore += OnRenderCore;
-            h_SetRenderTarget = new Hook(
-                typeof(GraphicsDevice).GetMethod("SetRenderTarget", new Type[] { typeof(RenderTarget2D) }),
-                new Action<Action<GraphicsDevice, RenderTarget2D>, GraphicsDevice, RenderTarget2D>(OnSetRenderTarget)
-            );
+            IL.Monocle.Engine.RenderCore += ILRenderCore;
         }
 
         protected override void Dispose(bool disposing) {
             base.Dispose(disposing);
 
             MainThreadHelper.Do(() => {
-                On.Celeste.Celeste.RenderCore -= OnRenderCore;
-                h_SetRenderTarget?.Dispose();
-            });
+                IL.Monocle.Engine.RenderCore -= ILRenderCore;
 
-            MainThreadHelper.Do(() => {
                 RTGame?.Dispose();
                 RTBlurX?.Dispose();
                 RTBlurY?.Dispose();
@@ -81,111 +74,24 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
             });
         }
 
-        private void OnRenderCore(On.Celeste.Celeste.orig_RenderCore orig, Celeste self) {
-            ForceRTGameAsBackbuffer = true;
-            Viewport viewportPrev = Engine.Viewport;
-            Engine.SetViewport(new Viewport(0, 0, Engine.ViewWidth, Engine.ViewHeight));
+        private void ILRenderCore(ILContext il) {
+            ILCursor c = new ILCursor(il);
 
-            orig(self);
+            VariableDefinition vd_tmpRealVP = new VariableDefinition(il.Import(typeof(Viewport)));
+            il.Body.Variables.Add(vd_tmpRealVP);
+            VariableDefinition vd_tmpRealRT = new VariableDefinition(il.Import(typeof(RenderTarget2D)));
+            il.Body.Variables.Add(vd_tmpRealRT);
 
-            ForceRTGameAsBackbuffer = false;
-            Engine.SetViewport(viewportPrev);
+            c.EmitDelegate<Func<Viewport>>(() => {
+                Viewport tmpRealVP = Engine.Viewport;
+                Engine.SetViewport(new Viewport(0, 0, Engine.ViewWidth, Engine.ViewHeight));
+                return tmpRealVP;
+            });
+            c.Emit(OpCodes.Stloc, vd_tmpRealVP);
 
-            if (RTGame != null) {
-                const int blurrad = 16;
-                const float blurdist = 0.6f;
-                Vector2 blurScale = new Vector2(BLUR_WIDTH / (float) viewportPrev.Width, BLUR_HEIGHT / (float) viewportPrev.Height);
-                Vector2 blurScaleInv = new Vector2(UI_WIDTH / (float) BLUR_WIDTH, UI_HEIGHT / (float) BLUR_HEIGHT);
-
-                GraphicsDevice.Viewport = new Viewport(0, 0, BLUR_WIDTH, BLUR_HEIGHT);
-
-                GraphicsDevice.SetRenderTarget(RTBlurX);
-                GraphicsDevice.Clear(Engine.ClearColor);
-                MDraw.SpriteBatch.Begin(
-                    SpriteSortMode.Deferred,
-                    BlendState.AlphaBlend,
-                    SamplerState.LinearClamp,
-                    DepthStencilState.None,
-                    RasterizerState.CullNone,
-                    null,
-                    Matrix.Identity
-                );
-
-                MDraw.SpriteBatch.Draw(RTGame, new Vector2(0f, 0f), null, Color.White, 0f, Vector2.Zero, blurScale, SpriteEffects.None, 0);
-
-                for (int x = blurrad - 1; x > 0; x--) {
-                    float a = (blurrad - x) * 0.5f / blurrad;
-                    Color c = new Color(a, a, a, a) * 0.5f;
-                    MDraw.SpriteBatch.Draw(RTGame, new Vector2(-x * blurdist - 0.5f, 0), null, c, 0f, Vector2.Zero, blurScale, SpriteEffects.None, 0);
-                    MDraw.SpriteBatch.Draw(RTGame, new Vector2(+x * blurdist - 0.5f, 0), null, c, 0f, Vector2.Zero, blurScale, SpriteEffects.None, 0);
-                }
-
-                MDraw.SpriteBatch.End();
-
-                GraphicsDevice.SetRenderTarget(RTBlurY);
-                GraphicsDevice.Clear(Engine.ClearColor);
-                MDraw.SpriteBatch.Begin(
-                    SpriteSortMode.Deferred,
-                    BlendState.AlphaBlend,
-                    SamplerState.LinearClamp,
-                    DepthStencilState.None,
-                    RasterizerState.CullNone,
-                    null,
-                    Matrix.Identity
-                );
-
-                MDraw.SpriteBatch.Draw(RTBlurX, new Vector2(0f, 0f), null, Color.White, 0f, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
-
-                for (int y = blurrad - 1; y > 0; y--) {
-                    float a = (blurrad - y) * 0.5f / blurrad;
-                    Color c = new Color(a, a, a, a) * 0.5f;
-                    MDraw.SpriteBatch.Draw(RTBlurX, new Vector2(0f, -y * blurdist - 0.5f), null, c, 0f, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
-                    MDraw.SpriteBatch.Draw(RTBlurX, new Vector2(0f, +y * blurdist - 0.5f), null, c, 0f, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
-                }
-
-                MDraw.SpriteBatch.End();
-
-                GraphicsDevice.SetRenderTarget(RTBlur);
-                GraphicsDevice.Clear(Engine.ClearColor);
-                MDraw.SpriteBatch.Begin(
-                    SpriteSortMode.Deferred,
-                    BlendState.Opaque,
-                    SamplerState.LinearClamp,
-                    DepthStencilState.None,
-                    RasterizerState.CullNone,
-                    null,
-                    Matrix.Identity
-                );
-
-                MDraw.SpriteBatch.Draw(RTBlurY, new Vector2(0f, 0f), null, Color.White, 0f, Vector2.Zero, blurScaleInv, SpriteEffects.None, 0);
-
-                MDraw.SpriteBatch.End();
-
-            }
-
-            GraphicsDevice.SetRenderTarget(null);
-            GraphicsDevice.Viewport = Engine.Viewport;
-            GraphicsDevice.Clear(Engine.ClearColor);
-
-            if (RTGame != null) {
-                MDraw.SpriteBatch.Begin(
-                    SpriteSortMode.Deferred,
-                    BlendState.Opaque,
-                    SamplerState.LinearClamp,
-                    DepthStencilState.None,
-                    RasterizerState.CullNone,
-                    null,
-                    Matrix.Identity
-                );
-
-                MDraw.SpriteBatch.Draw(RTGame, Vector2.Zero, Color.White);
-
-                MDraw.SpriteBatch.End();
-            }
-        }
-
-        private void OnSetRenderTarget(Action<GraphicsDevice, RenderTarget2D> orig, GraphicsDevice self, RenderTarget2D renderTarget) {
-            if (ForceRTGameAsBackbuffer && renderTarget == null) {
+            c.GotoNext(i => i.MatchCallOrCallvirt(typeof(GraphicsDevice).GetMethod("SetRenderTarget", new Type[] { typeof(RenderTarget2D) })));
+            c.Emit(OpCodes.Stloc, vd_tmpRealRT);
+            c.EmitDelegate<Func<RenderTarget2D>>(() => {
                 if (RTGame != null && (RTGame.Width != Engine.ViewWidth || RTGame.Height != Engine.ViewHeight)) {
                     RTGame.Dispose();
                     RTGame = null;
@@ -193,21 +99,117 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
 
                 if (RTGame == null) {
                     RTGame = new RenderTarget2D(
-                        self,
+                        GraphicsDevice,
                         Engine.ViewWidth,
                         Engine.ViewHeight,
                         false,
                         SurfaceFormat.Color,
-                        self.PresentationParameters.DepthStencilFormat,
+                        GraphicsDevice.PresentationParameters.DepthStencilFormat,
                         0,
                         RenderTargetUsage.DiscardContents
                     );
                 }
 
-                renderTarget = RTGame;
-            }
+                return RTGame;
+            });
 
-            orig(self, renderTarget);
+            c.GotoNext(i => i.MatchRet());
+            c.Emit(OpCodes.Ldloc, vd_tmpRealVP);
+            c.Emit(OpCodes.Ldloc, vd_tmpRealRT);
+            c.EmitDelegate<Action<Viewport, RenderTarget2D>>((tmpRealVP, tmpRealRT) => {
+                Engine.SetViewport(tmpRealVP);
+
+                if (RTGame != null) {
+                    const int blurrad = 16;
+                    const float blurdist = 0.6f;
+                    Vector2 blurScale = new Vector2(BLUR_WIDTH / (float) tmpRealVP.Width, BLUR_HEIGHT / (float) tmpRealVP.Height);
+                    Vector2 blurScaleInv = new Vector2(UI_WIDTH / (float) BLUR_WIDTH, UI_HEIGHT / (float) BLUR_HEIGHT);
+
+                    GraphicsDevice.Viewport = new Viewport(0, 0, BLUR_WIDTH, BLUR_HEIGHT);
+
+                    GraphicsDevice.SetRenderTarget(RTBlurX);
+                    GraphicsDevice.Clear(Engine.ClearColor);
+                    MDraw.SpriteBatch.Begin(
+                        SpriteSortMode.Deferred,
+                        BlendState.AlphaBlend,
+                        SamplerState.LinearClamp,
+                        DepthStencilState.None,
+                        RasterizerState.CullNone,
+                        null,
+                        Matrix.Identity
+                    );
+
+                    MDraw.SpriteBatch.Draw(RTGame, new Vector2(0f, 0f), null, Color.White, 0f, Vector2.Zero, blurScale, SpriteEffects.None, 0);
+
+                    for (int x = blurrad - 1; x > 0; x--) {
+                        float a = (blurrad - x) * 0.5f / blurrad;
+                        Color c = new Color(a, a, a, a) * 0.5f;
+                        MDraw.SpriteBatch.Draw(RTGame, new Vector2(-x * blurdist - 0.5f, 0), null, c, 0f, Vector2.Zero, blurScale, SpriteEffects.None, 0);
+                        MDraw.SpriteBatch.Draw(RTGame, new Vector2(+x * blurdist - 0.5f, 0), null, c, 0f, Vector2.Zero, blurScale, SpriteEffects.None, 0);
+                    }
+
+                    MDraw.SpriteBatch.End();
+
+                    GraphicsDevice.SetRenderTarget(RTBlurY);
+                    GraphicsDevice.Clear(Engine.ClearColor);
+                    MDraw.SpriteBatch.Begin(
+                        SpriteSortMode.Deferred,
+                        BlendState.AlphaBlend,
+                        SamplerState.LinearClamp,
+                        DepthStencilState.None,
+                        RasterizerState.CullNone,
+                        null,
+                        Matrix.Identity
+                    );
+
+                    MDraw.SpriteBatch.Draw(RTBlurX, new Vector2(0f, 0f), null, Color.White, 0f, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+
+                    for (int y = blurrad - 1; y > 0; y--) {
+                        float a = (blurrad - y) * 0.5f / blurrad;
+                        Color c = new Color(a, a, a, a) * 0.5f;
+                        MDraw.SpriteBatch.Draw(RTBlurX, new Vector2(0f, -y * blurdist - 0.5f), null, c, 0f, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+                        MDraw.SpriteBatch.Draw(RTBlurX, new Vector2(0f, +y * blurdist - 0.5f), null, c, 0f, Vector2.Zero, Vector2.One, SpriteEffects.None, 0);
+                    }
+
+                    MDraw.SpriteBatch.End();
+
+                    GraphicsDevice.SetRenderTarget(RTBlur);
+                    GraphicsDevice.Clear(Engine.ClearColor);
+                    MDraw.SpriteBatch.Begin(
+                        SpriteSortMode.Deferred,
+                        BlendState.Opaque,
+                        SamplerState.LinearClamp,
+                        DepthStencilState.None,
+                        RasterizerState.CullNone,
+                        null,
+                        Matrix.Identity
+                    );
+
+                    MDraw.SpriteBatch.Draw(RTBlurY, new Vector2(0f, 0f), null, Color.White, 0f, Vector2.Zero, blurScaleInv, SpriteEffects.None, 0);
+
+                    MDraw.SpriteBatch.End();
+                }
+
+                if (RTGame != null) {
+                    GraphicsDevice.SetRenderTarget(tmpRealRT);
+                    GraphicsDevice.Viewport = Engine.Viewport;
+                    GraphicsDevice.Clear(Engine.ClearColor);
+
+                    MDraw.SpriteBatch.Begin(
+                        SpriteSortMode.Deferred,
+                        BlendState.Opaque,
+                        SamplerState.LinearClamp,
+                        DepthStencilState.None,
+                        RasterizerState.CullNone,
+                        null,
+                        Matrix.Identity
+                    );
+
+                    MDraw.SpriteBatch.Draw(RTGame, Vector2.Zero, Color.White);
+
+                    MDraw.SpriteBatch.End();
+                }
+            });
         }
 
     }

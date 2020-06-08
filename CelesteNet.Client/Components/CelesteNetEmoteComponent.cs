77a344bf1be607d53fb0d939f5a22fff7936c3f3
Maik Macho <nuda1998@gmail.com>
2020-06-08 21:51:21 +0200

    Add keyboard emote support

--- CelesteNet.Client/Components/CelesteNetEmoteComponent.cs
+++ CelesteNet.Client/Components/CelesteNetEmoteComponent.cs
@@ -31,6 +31,12 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
             });
         }
 
+        public void Send(int index) {
+            string[] emotes = CelesteNetClientModule.Settings.Emotes;
+            if (0 <= index && index < emotes.Length)
+                Send(emotes[index]);
+        }
+
         public void Handle(CelesteNetConnection con, DataEmote emoteData) {
             Entity target;
 
@@ -75,14 +81,34 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
                 Wheel.Shown = CelesteNetClientModule.Instance.JoystickEmoteWheel.Value.LengthSquared() >= 0.36f;
                 int selected = Wheel.Selected;
                 if (Wheel.Shown && selected != -1 && CelesteNetClientModule.Instance.ButtonEmoteSend.Pressed) {
-                    string[] emotes = CelesteNetClientModule.Settings.Emotes;
-                    if (0 <= selected && selected < emotes.Length) {
-                        Send(emotes[selected]);
-                    }
+                    Send(selected);
                 }
             } else {
                 Wheel.Shown = false;
                 Wheel.Selected = -1;
+            }
+
+            if (!Context.Chat.Active) {
+                if (MInput.Keyboard.Pressed(Keys.D1))
+                    Send(0);
+                else if (MInput.Keyboard.Pressed(Keys.D2))
+                    Send(1);
+                else if (MInput.Keyboard.Pressed(Keys.D3))
+                    Send(2);
+                else if (MInput.Keyboard.Pressed(Keys.D4))
+                    Send(3);
+                else if (MInput.Keyboard.Pressed(Keys.D5))
+                    Send(4);
+                else if (MInput.Keyboard.Pressed(Keys.D6))
+                    Send(5);
+                else if (MInput.Keyboard.Pressed(Keys.D7))
+                    Send(6);
+                else if (MInput.Keyboard.Pressed(Keys.D8))
+                    Send(7);
+                else if (MInput.Keyboard.Pressed(Keys.D9))
+                    Send(8);
+                else if (MInput.Keyboard.Pressed(Keys.D0))
+                    Send(9);
             }
         }
 

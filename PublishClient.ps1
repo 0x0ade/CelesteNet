12c75ee37ee1c2a032dad71c2dc86ef88f167f75
Maik Macho <nuda1998@gmail.com>
2020-07-01 22:11:46 +0200

    Make PublishClient publish Release not Debug

--- PublishClient.ps1
+++ PublishClient.ps1
@@ -2,7 +2,7 @@
 Remove-Item -Path CelesteNet.Client.zip
 New-Item -ItemType directory -Path PubClient
 Copy-Item -Path everest.pubclient.yaml -Destination PubClient/everest.yaml
-Copy-Item -Recurse -Path CelesteNet.Client/bin/Debug/net452/* -Destination PubClient
+Copy-Item -Recurse -Path CelesteNet.Client/bin/Release/net452/* -Destination PubClient
 Copy-Item -Recurse -Path Dialog -Destination PubClient
 Copy-Item -Recurse -Path Graphics -Destination PubClient
 Compress-Archive -Path PubClient/* -DestinationPath CelesteNet.Client.zip

639e39481c8e6b91b596b51ed6122a53abe6ec94
Maik Macho <nuda1998@gmail.com>
2020-07-05 01:06:53 +0200

    Kill CelesteNet on GhostNet, not the opposite, as it's easier

--- CelesteNet.Client/Components/CelesteNetKillGhostNetComponent.cs
+++ CelesteNet.Client/Components/CelesteNetKillGhostNetComponent.cs
@@ -17,7 +17,7 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
     public class CelesteNetKillGhostNetComponent : CelesteNetGameComponent {
 
         public object GhostNetModule;
-        public MethodInfo m_GhostNetModule_Stop;
+        public FieldInfo m_GhostNetModule_Client;
 
         public CelesteNetKillGhostNetComponent(CelesteNetClientComponent context, Game game)
             : base(context, game) {
@@ -30,15 +30,17 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
                     continue;
 
                 GhostNetModule = module;
-                m_GhostNetModule_Stop = module.GetType().GetMethod("Stop");
+                m_GhostNetModule_Client = module.GetType().GetField("Client");
             }
         }
 
         public override void Update(GameTime gameTime) {
             base.Update(gameTime);
 
-            if (m_GhostNetModule_Stop != null)
-                m_GhostNetModule_Stop.Invoke(GhostNetModule, Dummy<object>.EmptyArray);
+            if (m_GhostNetModule_Client?.GetValue(GhostNetModule) != null) {
+                Context.Status.Set("Disconnected from CelesteNet, connected to GhostNet", 10, false);
+                Settings.Connected = false;
+            }
         }
 
     }

2664c3e93139e55cbc08ae8e76174e4160a9de2e
Popax21 <popa21@popa21.at>
2021-12-30 12:28:06 +0100

    Fixed thread role assignment resulting in thread loop exiting

--- CelesteNet.Server/NetPlus/ThreadPool.cs
+++ CelesteNet.Server/NetPlus/ThreadPool.cs
@@ -181,7 +181,6 @@ namespace Celeste.Mod.CelesteNet.Server {
         }
 
         private void ThreadLoop() {
-            CancellationToken? lastWorkerToken = null;
             try {
                 CancellationToken poolToken = Pool.Token;
                 poolToken.Register(() => RoleWorkerTokenSrc?.Cancel());
@@ -191,7 +190,6 @@ namespace Celeste.Mod.CelesteNet.Server {
                     // Start the worker
                     using (_RoleWorker = _Role.CreateWorker(this))
                     using (RoleWorkerTokenSrc = new()) {
-                        lastWorkerToken = RoleWorkerTokenSrc.Token;
                         RoleSwitchSem.Release();
                         try {
                             while (!RoleWorkerTokenSrc.IsCancellationRequested) {
@@ -199,6 +197,9 @@ namespace Celeste.Mod.CelesteNet.Server {
                                 if (!RoleWorkerTokenSrc.IsCancellationRequested)
                                     Logger.Log(LogLevel.WRN, "netplus", $"Thread pool thread {Index} worker {_RoleWorker} exited prematurely!");
                             }
+                        } catch (Exception e) {
+                            if (!(e is OperationCanceledException ce && ce.CancellationToken == RoleWorkerTokenSrc.Token))
+                                throw;
                         } finally {
                             _RoleWorker.ActiveZoneCounter = 0;
                             _RoleWorker = null;
@@ -207,8 +208,6 @@ namespace Celeste.Mod.CelesteNet.Server {
                     }
                 }
             } catch (Exception e) {
-                if (e is OperationCanceledException ce && ce.CancellationToken == lastWorkerToken) return;
-
                 // Report error to the pool
                 Pool.ReportThreadError(this, e);
             } finally {

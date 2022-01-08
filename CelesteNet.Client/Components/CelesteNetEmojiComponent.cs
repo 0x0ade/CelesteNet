a0c59fc9a369264c80545f1287fe06fc875de8f1
Jade Macho <nuda1998@gmail.com>
2022-01-08 21:07:27 +0100

    Cleanup

--- CelesteNet.Client/Components/CelesteNetEmojiComponent.cs
+++ CelesteNet.Client/Components/CelesteNetEmojiComponent.cs
@@ -18,27 +18,28 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
 
         public class NetEmojiContent : ModContent {
 
+            public HashSet<string> Registered = new();
+
             protected override void Dispose(bool disposing) {
-                foreach (ModAsset asset in this.List) {
+                foreach (ModAsset asset in List) {
                     if (asset is NetEmojiAsset netemoji)
                         netemoji.Dispose();
                 }
                 base.Dispose(disposing);
             }
 
-            public HashSet<string> Registered = new();
             protected override void Crawl() {}
 
         }
 
         public class NetEmojiAsset : ModAsset, IDisposable {
 
-            private int nextSeq = 0;
-            private MemoryStream memStream = new();
+            private int NextSeq = 0;
+            private readonly MemoryStream Stream = new();
 
             public new NetEmojiContent Source => (NetEmojiContent) base.Source;
             public readonly string ID;
-            public bool Pending => nextSeq >= 0;
+            public bool Pending => NextSeq >= 0;
 
             public NetEmojiAsset(NetEmojiContent content, string id) : base(content) {
                 ID = id;
@@ -48,7 +49,7 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
             }
 
             public void Dispose() {
-                memStream.Dispose();
+                Stream.Dispose();
                 if (!Pending) {
                     Emoji.Register(ID, GFX.Misc["whiteCube"]);
                     Emoji.Fill(CelesteNetClientFont.Font);
@@ -61,16 +62,16 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
                     throw new InvalidOperationException($"NetEmoji '{ID}' isn't pending!");
 
                 // Check and increase the sequence number
-                if (data.SequenceNumber != nextSeq)
-                    throw new InvalidOperationException($"Unexpected NetEmoji '{ID}' sequence number: expected {nextSeq}, got {data.SequenceNumber}!");
-                nextSeq = (nextSeq + 1) % DataNetEmoji.MaxSequenceNumber;
+                if (data.SequenceNumber != NextSeq)
+                    throw new InvalidOperationException($"Unexpected NetEmoji '{ID}' sequence number: expected {NextSeq}, got {data.SequenceNumber}!");
+                NextSeq = (NextSeq + 1) % DataNetEmoji.MaxSequenceNumber;
 
                 // Add fragment data to the emoji
-                memStream.Write(data.Data, 0, data.Data.Length);
+                Stream.Write(data.Data, 0, data.Data.Length);
 
                 // Check if there are more fragments
                 if (!data.MoreFragments)
-                    nextSeq = -1;
+                    NextSeq = -1;
             }
 
             protected override void Open(out Stream stream, out bool isSection) {
@@ -79,14 +80,14 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
                     throw new InvalidOperationException($"NetEmoji '{ID}' is pending!");
 
                 // Don't duplicate the memory
-                stream = new MemoryStream(memStream.GetBuffer());
+                stream = new MemoryStream(Stream.GetBuffer());
                 isSection = false;
             }
 
         }
 
         public NetEmojiContent Content = new();
-        private Dictionary<string, NetEmojiAsset> Pending = new();
+        private readonly Dictionary<string, NetEmojiAsset> Pending = new();
 
         public CelesteNetEmojiComponent(CelesteNetClientContext context, Game game)
             : base(context, game) {
@@ -109,7 +110,7 @@ namespace Celeste.Mod.CelesteNet.Client.Components {
                     Pending.Remove(netemoji.ID);
 
                     // Register the emoji
-                    MTexture tex = new MTexture(VirtualContent.CreateTexture(asset)); 
+                    MTexture tex = new(VirtualContent.CreateTexture(asset));
                     Content.Registered.Add(asset.ID);
                     Emoji.Register(asset.ID, tex);
                     Emoji.Fill(CelesteNetClientFont.Font);

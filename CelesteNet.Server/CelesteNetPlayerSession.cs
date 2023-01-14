22724391ff6c151ccdb17a9122a55b5cb820dcd5
RedFlames <simon.thyssen@gmx.de>
2023-01-14 21:58:34 +0100

    More logging about how many blobs get sent around before DataReady gets sent

--- CelesteNet.Server/CelesteNetPlayerSession.cs
+++ CelesteNet.Server/CelesteNetPlayerSession.cs
@@ -168,9 +168,15 @@ namespace Celeste.Mod.CelesteNet.Server {
             DataInternalBlob blobPlayerInfo = DataInternalBlob.For(Server.Data, playerInfo);
 
             Con.Send(playerInfo);
+            Logger.Log(LogLevel.VVV, "playersession", $"Session #{SessionID} - Sent own PlayerInfo");
+
             foreach (DataInternalBlob fragBlob in AvatarFragments)
                 Con.Send(fragBlob);
+            Logger.Log(LogLevel.VVV, "playersession", $"Session #{SessionID} - Sent own Avatar frags");
 
+            int blobSendsNew = 0, avaSendsNew = 0;
+            int blobSendsOut = 0, avaSendsOut = 0;
+            int boundSends = 0;
             using (Server.ConLock.R())
                 foreach (CelesteNetPlayerSession other in Server.Sessions) {
                     if (other == this)
@@ -181,17 +187,27 @@ namespace Celeste.Mod.CelesteNet.Server {
                         continue;
 
                     other.Con.Send(blobPlayerInfo);
-                    foreach (DataInternalBlob fragBlob in AvatarFragments)
+                    blobSendsOut++;
+                    foreach (DataInternalBlob fragBlob in AvatarFragments) {
                         other.Con.Send(fragBlob);
+                        avaSendsOut++;
+                    }
 
                     Con.Send(otherInfo);
-                    foreach (DataInternalBlob fragBlob in other.AvatarFragments)
+                    blobSendsNew++;
+                    foreach (DataInternalBlob fragBlob in other.AvatarFragments) {
                         Con.Send(fragBlob);
+                        avaSendsNew++;
+                    }
 
                     foreach (DataType bound in Server.Data.GetBoundRefs(otherInfo))
-                        if (!bound.Is<MetaPlayerPrivateState>(Server.Data) || other.Channel.ID == 0)
+                        if (!bound.Is<MetaPlayerPrivateState>(Server.Data) || other.Channel.ID == 0) {
                             Con.Send(bound);
+                            boundSends++;
+                        }
                 }
+
+            Logger.Log(LogLevel.VVV, "playersession", $"Session #{SessionID} - Done using ConLock -- blobSendsNew/avaSendsNew {blobSendsNew}/{avaSendsNew} - blobSendsOut/avaSendsOut {blobSendsOut}/{avaSendsOut} - boundSends {boundSends}");
 
             ResendPlayerStates();
 
@@ -250,25 +266,36 @@ namespace Celeste.Mod.CelesteNet.Server {
             IEnumerable<DataInternalBlob> boundAllPublic = boundAll[false];
             IEnumerable<DataInternalBlob> boundAllPrivate = boundAll[true];
 
+            Logger.Log(LogLevel.VVV, "playersession", $"Session #{SessionID} - Doing player state resends");
+            int boundPrivOut = 0, boundPublicOut = 0, boundPrivNew = 0;
             using (Server.ConLock.R())
                 foreach (CelesteNetPlayerSession other in Server.Sessions) {
                     if (other == this)
                         continue;
 
-                    foreach (DataType bound in boundAllPublic)
+                    foreach (DataType bound in boundAllPublic) {
                         other.Con.Send(bound);
-                    foreach (DataType bound in boundAllPrivate)
-                        if (channel == other.Channel)
+                        boundPublicOut++;
+                    }
+                    foreach (DataType bound in boundAllPrivate) {
+                        if (channel == other.Channel) {
                             other.Con.Send(bound);
+                            boundPrivOut++;
+                        }
+                    }
 
                     DataPlayerInfo? otherInfo = other.PlayerInfo;
                     if (otherInfo == null)
                         continue;
 
                     foreach (DataType bound in Server.Data.GetBoundRefs(otherInfo))
-                        if (!bound.Is<MetaPlayerPrivateState>(Server.Data) || channel == other.Channel)
+                        if (!bound.Is<MetaPlayerPrivateState>(Server.Data) || channel == other.Channel) {
                             Con.Send(bound);
+                            boundPrivNew++;
+                        }
                 }
+            Logger.Log(LogLevel.VVV, "playersession", $"Session #{SessionID} - Done resends -- boundPrivOut/boundPublicOut {boundPrivOut}/{boundPublicOut} - boundPrivNew {boundPrivNew}");
+
         }
 
         public bool IsSameArea(CelesteNetPlayerSession other)

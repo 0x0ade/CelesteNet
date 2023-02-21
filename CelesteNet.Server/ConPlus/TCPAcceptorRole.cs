6db44800828e90d5aab36a93d106ee193d36b72c
RedFlames <simon.thyssen@gmx.de>
2023-02-21 13:35:59 +0100

    Resolve some compiler warnings.

--- CelesteNet.Server/ConPlus/TCPAcceptorRole.cs
+++ CelesteNet.Server/ConPlus/TCPAcceptorRole.cs
@@ -44,7 +44,7 @@ namespace Celeste.Mod.CelesteNet.Server {
                     Role.Handshaker.Factory.StartNew(() => {
                         Role.Handshaker.DoTCPUDPHandshake(newCon, Role.ConnectionSettings, Role.TCPReceiver, Role.UDPReceiver, Role.Sender).ContinueWith(t => {
                             if (t.IsFaulted) {
-                                if (t.Exception.InnerException is SocketException se && se.IsDisconnect())
+                                if (t.Exception?.InnerException is SocketException se && se.IsDisconnect())
                                     Logger.Log(LogLevel.WRN, "tcpaccept", $"Disconnect during handshake for connection {remoteEP}");
                                 else
                                     Logger.Log(LogLevel.WRN, "tcpaccept", $"Handshake failed for connection {remoteEP}: {t.Exception}");

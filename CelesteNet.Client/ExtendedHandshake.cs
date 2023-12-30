9e91b259472da1f000085215c46e67fb9f86c311
RedFlames <simon.thyssen@gmx.de>
2023-12-30 20:05:56 +0100

    Move ExtendedHandshake to separate file instead of putting it in Handshake.cs in Client

---
+++ CelesteNet.Client/ExtendedHandshake.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.CelesteNet {
+
+    public class ExtendedHandshake : IConnectionFeature {
+
+        public Task DoHandshake(CelesteNetConnection con, bool isClient) {
+            if (!isClient)
+                throw new InvalidOperationException($"HandshakeFeature was called with 'isClient == false' in Client context!");
+
+            // Handler for DataClientInfoRequest is implemented in CelesteNetClient
+
+            return Task.Run(() => {
+                Logger.Log(LogLevel.VVV, "handshakeFeature", $"Connection uses ExtendedHandshake");
+            });
+        }
+
+        public void Register(CelesteNetConnection con, bool isClient) {
+            if (!isClient)
+                throw new InvalidOperationException($"HandshakeFeature was called with 'isClient == false' in Client context!");
+        }
+    }
+}

a369d7a4127fe5715bf4ebcaa14fc37985bcbc18
RedFlames <simon.thyssen@gmx.de>
2024-04-28 20:51:48 +0200

    Integrate Extras module into main repo

--- PublishClient.sh
+++ PublishClient.sh
@@ -5,9 +5,6 @@ mkdir PubClient
 cp everest.pubclient.yaml PubClient/everest.yaml
 cp -r CelesteNet.Client/bin/Release/net7.0/CelesteNet.* PubClient
 [ -f PubClient/CelesteNet.Client.deps.json ] && rm PubClient/CelesteNet.Client.deps.json
-if [ -f CelesteNet.Extras/bin/Release/net7.0/CelesteNet.Extras.dll ]; then
-    cp CelesteNet.Extras/bin/Release/net7.0/CelesteNet.Extras.dll PubClient
-fi
 cp -r Dialog PubClient
 cp -r Graphics PubClient
 cd PubClient; zip -r ../CelesteNet.Client.zip *

e962823cf9666024fd255db9cb5d72a3a5c4d7c6
Simon Thyßen <simon.thyssen@gmx.de>
2025-09-15 02:16:15 +0200

    Upgrade all projects to net8.0 target fw, require Everest 5577+, bump some patch/minor releases of package deps (#180)

--- PublishClient.sh
+++ PublishClient.sh
@@ -3,7 +3,7 @@ dotnet build CelesteNet.Client -c Release
 rm -rf PubClient CelesteNet.Client.zip
 mkdir PubClient
 cp everest.pubclient.yaml PubClient/everest.yaml
-cp -r CelesteNet.Client/bin/Release/net7.0/CelesteNet.* PubClient
+cp -r CelesteNet.Client/bin/Release/net8.0/CelesteNet.* PubClient
 [ -f PubClient/CelesteNet.Client.deps.json ] && rm PubClient/CelesteNet.Client.deps.json
 cp -r Dialog PubClient
 cp -r Graphics PubClient
